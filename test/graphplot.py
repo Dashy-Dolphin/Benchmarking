5b118fa56dc10bd3fea13a4a9520449511c0d8e3
Sasi <shashankyeluri@gmail.com>
2023-04-02 12:42:58 +0530

    temp

--- test/graphplot.py
+++ test/graphplot.py
@@ -8,9 +8,8 @@ n_counter = 1
 
 
 xpoints = [1,2,3,4,5,8,10,13,15,18,20,23,25,27,30,33, 35]
-#xpoints = [1]
 ypoints = []
-sleep= [100, 500, 1000]
+sleep= [85]
 
 for t1 in sleep:
     ypoints = []
@@ -63,27 +62,3 @@ for t1 in sleep:
 
 
 
-
-
-
-
-# mode = 'obstruction-free'
-# ypoints = []
-
-# for x in xpoints:
-#     print(x)
-#     n_counter = x
-#     proc = subprocess.Popen(["hyperfine --warmup 2 \"dune exec ./tx_loc_modes.exe " + str(loop_count) + " " + mode + " " +str(n_counter) +" " + str(sleep_time)+"\" -i" ], stdout=subprocess.PIPE, shell=True)
-#     (out, err) = proc.communicate()
-
-#     out = out.decode()
-#     value = out.split('\n')[1].split()[4:6]
-#     value[0] = float(value[0])
-#     if (value[1] != 'ms'):
-#         value[0]  *= 1000
-#     ypoints.append(value[0])
-
-
-# print("\nobstruction-free mode\n")
-# print("xpoints = ",xpoints)
-# print("ypoints = ", ypoints)
