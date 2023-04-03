aa38e9be73593a9ce1069a24cf1ccb21e85930b3
Sasi <shashankyeluri@gmail.com>
2023-04-03 12:08:12 +0530

    hybrid

--- test/cache_ref.sh
+++ test/cache_ref.sh
@@ -5,7 +5,7 @@ n_counter_list="1 2 3 4 5 8 10 13 15 18 20 23 25 27 30 33 35"
 sleep_time=85
 
 
-for n_counter in n_counter_list
+for n_counter in $n_counter_list
 do 
     perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations dune exec ./tx_loc_modes.exe $loop_count $mode $n_counter $t1 -i
 done
