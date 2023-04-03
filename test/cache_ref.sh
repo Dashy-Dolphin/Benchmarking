16c5e5e9a6b9fe5c5327b33017e30aa891845c62
Sasi <shashankyeluri@gmail.com>
2023-04-03 12:04:57 +0530

    hybrid

---
+++ test/cache_ref.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+loop_count=20000
+mode='lock-free'
+n_counter_list="1 2 3 4 5 8 10 13 15 18 20 23 25 27 30 33 35"
+sleep_time=85
+
+
+for n_counter in n_counter_list
+do 
+    perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations dune exec ./tx_loc_modes.exe $loop_count $mode $n_counter $t1 -i
+done
