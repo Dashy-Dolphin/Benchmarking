1fcea9d6200f3ade6a7130d7439080f052531b6b
Sasi <shashankyeluri@gmail.com>
2023-04-04 11:10:27 +0530

    hybrid_new

--- test/cacheplot.py
+++ test/cacheplot.py
@@ -6,6 +6,7 @@ mode = 'obstruction-free'
 n_counter = 1
 
 
+perf_count = 30
 
 xpoints = [1,2,3,4,5,8,10,13,15,18,20,23,25,27,30,33, 35]
 ypoints = []
@@ -17,24 +18,30 @@ for t1 in sleep:
     for x in xpoints:
         print(x)
         n_counter = x
-        proc = subprocess.Popen(["perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations  ../_build/default/test/tx_loc_modes.exe " + str(loop_count) + " " + mode + " " +str(n_counter) +" " + str(t1) ], stdout=subprocess.PIPE,stderr = subprocess.PIPE, shell=True)
-      
-        (out, err) = proc.communicate()
 
-       
-      
-        err = err.decode()
-     
+        avg_miss_rate = float(0.0)
+
+        for lp in range(perf_count):
+            proc = subprocess.Popen(["perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations  ../_build/default/test/tx_loc_modes.exe " + str(loop_count) + " " + mode + " " +str(n_counter) +" " + str(t1) ], stdout=subprocess.PIPE,stderr = subprocess.PIPE, shell=True)
         
-    
-        err = err.split('\n')
-     
-        err = err[4]
-        err = err.split(' ')
-        err = err[36]
-        print(err)
-        input()
+            (out, err) = proc.communicate()
 
+        
+        
+            err = err.decode()
+        
+            
+        
+            err = err.split('\n')
+        
+            err = err[4]
+            err = err.split(' ')
+            err = err[36]
+            print(err)
+            avg_miss_rate = avg_miss_rate + float(err)
+        
+        avg_miss_rate = avg_miss_rate/perf_count
+        ypoints.append(avg_miss_rate)
 
     print("xpoints = ",xpoints)
     print("ypoints = ", ypoints)
