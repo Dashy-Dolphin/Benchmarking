1a38d9ec87f7d801fa8d537c14154fa7fe608202
Sasi <shashankyeluri@gmail.com>
2023-04-06 19:43:17 +0530

    hybrid_new

--- src/kcas.ml
+++ src/kcas.ml
@@ -609,8 +609,7 @@ module Xt = struct
           let current = eval state' in
           if current != state.before then exit () else CASN (loc, state', l, r)
 
-  let obstruction_free_depth = 1
-  
+  let obstruction_free_depth = 0
 
   let rec commit ?(count = 0) backoff (mode : Mode.t) scheduler_opt tx =
     let xt =
--- test/graphplot.py
+++ test/graphplot.py
@@ -9,14 +9,14 @@ n_counter = 1
 
 xpoints = [1,2,3,4,5,8,10,13,15,18,20,23,25,27,30,33, 35]
 ypoints = []
-sleep = [85,150,250, 350, 450 , 550, 650, 750, 850, 950]
+sleep = [85,150,250, 350, 450 , 550, 650, 750, 850, 950, 1050]
+n_counter = 35
+ypoints = []
 
 for t1 in sleep:
-    ypoints = []
-    print ("obstruction-free with sleep time = ", t1, "ms")
-    for x in xpoints:
-        print(x)
-        n_counter = x
+      
+        print ("obstruction-free with sleep time = ", t1, "ms")
+      
         proc = subprocess.Popen(["hyperfine --warmup 2 \"dune exec ./tx_loc_modes.exe " + str(loop_count) + " " + mode + " " +str(n_counter) +" " + str(t1)+"\" -i" ], stdout=subprocess.PIPE, shell=True)
       
         (out, err) = proc.communicate()
@@ -30,41 +30,33 @@ for t1 in sleep:
         ypoints.append(value[0])
 
 
-    print("xpoints = ",xpoints)
-    print("ypoints = ", ypoints)
+
+print("xpoints = ",sleep)
+print("ypoints = ", ypoints)
+
+mode = 'lock-free'
 
 
-# mode = 'lock-free'
-
-
-
-# for t1 in sleep:
-#     ypoints = []
-#     print ("lock-free with sleep time = ", t1, "ms")
-#     for x in xpoints:
-#         print(x)
-#         n_counter = x
-#         proc = subprocess.Popen(["hyperfine --warmup 2 \"dune exec ./tx_loc_modes.exe " + str(loop_count) + " " + mode + " " +str(n_counter) +" " + str(t1)+"\" -i" ], stdout=subprocess.PIPE, shell=True)
+ypoints = []
+for t1 in sleep:
+        
+        print ("lock-free with sleep time = ", t1, "ms")
       
-#         (out, err) = proc.communicate()
+        proc = subprocess.Popen(["hyperfine --warmup 2 \"dune exec ./tx_loc_modes.exe " + str(loop_count) + " " + mode + " " +str(n_counter) +" " + str(t1)+"\" -i" ], stdout=subprocess.PIPE, shell=True)
+      
+        (out, err) = proc.communicate()
 
        
-#         out = out.decode()
-#         value = out.split('\n')[1].split()[4:6]
-#         value[0] = float(value[0])
-#         if (value[1] != 'ms'):
-#             value[0]  *= 1000
-#         ypoints.append(value[0])
+        out = out.decode()
+        value = out.split('\n')[1].split()[4:6]
+        value[0] = float(value[0])
+        if (value[1] != 'ms'):
+            value[0]  *= 1000
+        ypoints.append(value[0])
 
 
-
-
-#     print("xpoints = ",xpoints)
-#     print("ypoints = ", ypoints)
-
-
-
-    
+print("xpoints = ",sleep)
+print("ypoints = ", ypoints)
 
 
 
