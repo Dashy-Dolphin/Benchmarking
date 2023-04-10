5b0dc52362457e4dee76c06e243e9897157d3f2a
Sasi <shashankyeluri@gmail.com>
2023-04-10 22:09:32 +0530

    hybrid_new

--- test/cacheplot.py
+++ test/cacheplot.py
@@ -62,3 +62,44 @@ print("xpoints = ",sleep)
 print("ypoints = ", ypoints)
 
 
+mode = 'lock-free'
+
+
+for x in sleep:
+   
+        print ("lock-free with sleep time = ", x, "ms")
+    
+      
+        avg_miss_rate = float(0.0)
+
+        for lp in range(perf_count):
+            proc = subprocess.Popen(["perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations  dune exec ./tx_loc_modes.exe " + str(loop_count) + " " + mode + " " +str(n_counter) +" "+ str(n_accumulators) + " " + str(x) ], stdout=subprocess.PIPE,stderr = subprocess.PIPE, shell=True)
+        
+            (out, err) = proc.communicate()
+
+        
+        
+            err = err.decode()
+        
+            
+        
+            err = err.split('\n')
+            
+            err = err[6]
+            err = err.split(' ')
+            i= 0
+            while 1:
+                if err[i] == '%':
+                    break
+
+                i=i+1
+
+            err = err[i - 1]
+           
+            avg_miss_rate = avg_miss_rate + float(err)
+        
+        avg_miss_rate = avg_miss_rate/perf_count
+        ypoints.append(avg_miss_rate)
+
+print("xpoints = ",sleep)
+print("ypoints = ", ypoints)
