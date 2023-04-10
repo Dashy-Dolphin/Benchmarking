73afca37ee0aaa52fdea47343a9a8decbc8ea3a7
Sasi <shashankyeluri@gmail.com>
2023-04-10 21:39:48 +0530

    hybrid_new

--- test/graphplot.py
+++ test/graphplot.py
@@ -2,30 +2,25 @@ import subprocess
 import sys
 
 loop_count = 200000
-mode = 'obstruction-free'
-n_counter = 1
-
-
-
-ypoints = []
-
 n_counter = sys.argv[1]
 n_accumulators = sys.argv[2]
 
 left = int(sys.argv[3])
 right = int( sys.argv[4])
-sleep = []
 
+
+sleep = []
 while (left != right):
       sleep.append(left)
       left = int(left) + 50
 
 sleep.append(right)
-
 sleep = sleep[::-1]
       
 
 ypoints = []
+
+mode = 'obstruction-free'
 
 for t1 in sleep:
       
@@ -48,29 +43,29 @@ for t1 in sleep:
 print("xpoints = ",sleep)
 print("ypoints = ", ypoints)
 
-mode = 'lock-free'
+# mode = 'lock-free'
 
 
-ypoints = []
-for t1 in sleep:
+# ypoints = []
+# for t1 in sleep:
         
-        print ("lock-free with sleep time = ", t1, "ms")
+#         print ("lock-free with sleep time = ", t1, "ms")
       
-        proc = subprocess.Popen(["hyperfine --warmup 2 \"dune exec ./tx_loc_modes.exe " + str(loop_count) + " " + mode + " " +str(n_counter) +" "+ str(n_accumulators)+" " + str(t1)+"\" -i" ], stdout=subprocess.PIPE, shell=True)
+#         proc = subprocess.Popen(["hyperfine --warmup 2 \"dune exec ./tx_loc_modes.exe " + str(loop_count) + " " + mode + " " +str(n_counter) +" "+ str(n_accumulators)+" " + str(t1)+"\" -i" ], stdout=subprocess.PIPE, shell=True)
       
-        (out, err) = proc.communicate()
+#         (out, err) = proc.communicate()
 
        
-        out = out.decode()
-        value = out.split('\n')[1].split()[4:6]
-        value[0] = float(value[0])
-        if (value[1] != 'ms'):
-            value[0]  *= 1000
-        ypoints.append(value[0])
+#         out = out.decode()
+#         value = out.split('\n')[1].split()[4:6]
+#         value[0] = float(value[0])
+#         if (value[1] != 'ms'):
+#             value[0]  *= 1000
+#         ypoints.append(value[0])
 
 
-print("xpoints = ",sleep)
-print("ypoints = ", ypoints)
+# print("xpoints = ",sleep)
+# print("ypoints = ", ypoints)
 
 
 
